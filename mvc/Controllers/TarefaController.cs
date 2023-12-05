6c0f9f6370f31bf9eba43d35b651d5aad07ee7e2
rodrigoxaviersantos <rodrigoxaviersantoss@gmail.com>
2023-12-05 14:50:36 -0300

    Add project files.

---
+++ mvc/Controllers/TarefaController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class TarefaController : Controller
+    {
+        private static List<Tarefa> tarefas = new List<Tarefa>();
+
+        public IActionResult Index()
+        {
+            return View(tarefas);
+        }
+
+        // Get
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Tarefa tarefa)
+        {
+            tarefa.Id = tarefas.Count + 1;
+            tarefas.Add(tarefa);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Detalhes(int id)
+        {
+            Tarefa tarefa = tarefas.Find(t => t.Id == id);
+
+            return View(tarefa);
+        }
+
+        public IActionResult Editar(int id)
+        {
+            Tarefa tarefa = tarefas.Find(t => t.Id == id);
+            return View(tarefa);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Tarefa tarefa)
+        {
+            Tarefa tarefaExistente = tarefas.Find(t => t.Id == tarefa.Id);
+            tarefaExistente.Titulo = tarefa.Titulo;
+            tarefaExistente.Concluida = tarefa.Concluida;
+            tarefaExistente.DataInicio = tarefa.DataInicio;
+            tarefaExistente.DataFim = tarefa.DataFim;
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Deletar(int id)
+        {
+            Tarefa tarefa = tarefas.Find(t => t.Id == id);
+            return View(tarefa);
+        }
+
+        [HttpPost]
+        public IActionResult DeletarConfirmado(int id)
+        {
+            Tarefa tarefa = tarefas.Find(t => t.Id == id);
+            tarefas.Remove(tarefa);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ mvc/Models/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace mvc.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public bool Concluida { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+    }
+}
